8e2c9222cd9a5209a72b1227bded61583d2e8c40
IvanPragma <ip20060226@gmail.com>
2022-06-12 23:56:17 +0500

    PyPI package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+version = '0.1.3'
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ms-launcher',
+    version=version,
+    license='BSD-3-Clause license',
+    author='Ivan Ms',
+    author_email='ivanms.ept@gmail.com',
+    description='BombSquad plugin that filters the list of game servers and simplifies access to them',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Ms-company-BombSquad/ms-launcher',
+    download_url=f'https://github.com/Ms-company-BombSquad/ms-launcher/archive/v{version}.zip',
+    packages=['ms_launcher'],
+)
