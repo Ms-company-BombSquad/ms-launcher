fdc021032c6935fb231862b008cf64a861ee5495
IvanPragma <ip20060226@gmail.com>
2022-06-14 00:42:37 +0500

    Simplified the translation code
    - simplified the transition code
    - add "import-error" to pylint ignore

--- ms_launcher/tools/translation.py
+++ ms_launcher/tools/translation.py
@@ -1,33 +1,23 @@
-import ba
 import json
 
-
-class Trans:
-    """Base class for get translations.
-    Cache loaded language files to Trans.languages
-    """
-
-    languages = {}
-    languages_dir = 'ms_launcher/data/languages/'
-
-    def __getattr__(self, key: str):
-        language = ba.LanguageSubsystem().language.lower()
-        if language in self.languages:
-            try:
-                with open(self.languages_dir + language + '.json', 'r', encoding='utf-8') as f:
-                    self.languages[language] = json.loads(f.read())
-            except FileNotFoundError:
-                # Loading english language
-                with open(self.languages_dir + 'english.json', 'r', encoding='utf-8') as f:
-                    self.languages[language] = json.loads(f.read())
-
-        return self.languages[language].get(key)
+import ba
 
 
-_trans = Trans()
+languages = {}
+LANGUAGES_DIR = 'ms_launcher/data/languages/'
 
 
 def gettext(key: str, *args, **kwargs) -> str:
     """Get translated text by key."""
 
-    return _trans.get(key, key).format(*args, **kwargs)
+    language = ba.LanguageSubsystem().language.lower()
+    if language not in languages:
+        try:
+            with open(LANGUAGES_DIR + language + '.json', 'r', encoding='utf-8') as stream:
+                languages[language] = json.loads(stream.read())
+        except FileNotFoundError:
+            # Loading english language
+            with open(LANGUAGES_DIR + 'english.json', 'r', encoding='utf-8') as stream:
+                languages[language] = json.loads(stream.read())
+
+    return languages[language].get(key, key).format(*args, **kwargs)
